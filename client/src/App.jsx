5b0628e756b85a8e2b97caf999ed11f36e049f51
mohdameen17 <mohdameen090@gmail.com>
2026-05-10 20:27:55 +0530

    project complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,5 @@
-// import { useEffect, useState } from "react";
-// import reactLogo from "./assets/react.svg";
-// import viteLogo from "./assets/vite.svg";
-// import heroImg from "./assets/hero.png";
-// import "./App.css";
 import axios from "axios";
 
-// function App() {
-//   const [count, setCount] = useState(0);
-
-//   const [message, setMessage] = useState("");
-
-//   useEffect(() => {
-//     axios
-//       .get("http://localhost:5000/")
-//       .then((res) => setMessage(res.data))
-//       .catch((err) => console.log(err));
-//   }, []);
-//   return (
-//     <>
-//       <div>
-//         <h1>MERN Project</h1>
-//         <p>{message}</p>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default App;
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import { AuthProvider } from "./context/AuthContext";
