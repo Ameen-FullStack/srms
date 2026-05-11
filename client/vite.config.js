211f087dd1656aad33fe4aab17e7cf0fa43b5b18
mohdameen17 <mohdameen090@gmail.com>
2026-05-12 00:22:38 +0530

    url change

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000",
+        target: "https://srms-backend-hsko.onrender.com/",
         changeOrigin: true,
       },
     },
